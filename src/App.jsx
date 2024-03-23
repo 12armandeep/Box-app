43e2b17acb9d3b44576dd672e04369cf6aed0e0c
Arman <saroyasabb798@outlook.com>
2024-03-23 13:46:24 +0530

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Started from './pages/started';
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    //client side routing library is route
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/started" element={<Started />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/started.jsx
@@ -0,0 +1,5 @@
+const Started = () => {
+  return <div>Start page</div>;
+};
+
+export default Started;
