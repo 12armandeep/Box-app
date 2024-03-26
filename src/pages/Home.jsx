2061a98d6c64ed531ad6db9820f37230b3b1fdb9
Arman <saroyasabb798@outlook.com>
2024-03-26 11:37:32 +0530

    search for movies or shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+  const onInputChange = ev => {
+    setInputValue(ev.target.value);
+  };
+
+  const onSearch = async event => {
+    event.preventDefault();
+
+    const response = await fetch(
+      ` https://api.tvmaze.com/search/shows?q=${inputValue}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
   return (
     <div>
-      <Link to={'/started'}>go to srart page</Link>
+      <form onSubmit={onSearch}>
+        <input type="text" value={inputValue} onChange={onInputChange} />
+        {/* <Link to={'/started'}>go to srart page</Link>  */}
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 };
