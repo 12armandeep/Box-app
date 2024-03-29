84aaa09e6b5771461349ddfbd96b2bc1cb39ba01
Arman <saroyasabb798@outlook.com>
2024-03-29 16:34:15 +0530

    install react query and replace it with the usefetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import Home from './pages/Home';
 import Started from './pages/started';
 import MainLayout from './components/mainLayout';
 import Show from './pages/show';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     //client side routing library is route
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/started" element={<Started />} />
-        </Route>
-        <Route path="/show/:showId" element={<Show />} />
-        <Route path="*" element={<div>Not found</div>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/started" element={<Started />} />
+          </Route>
+          <Route path="/show/:showId" element={<Show />} />
+          <Route path="*" element={<div>Not found</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/show.jsx
+++ src/pages/show.jsx
@@ -1,23 +1,33 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getElementById } from '../api/tvmaze';
+import { useQuery } from '@tanstack/react-query';
+
+// const useShowById = showId => {
+//   const [showData, setShowData] = useState(null);
+//   const [ShowError, setShowError] = useState(null);
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const data = await getElementById(showId);
+//         setShowData(data);
+//       } catch (err) {
+//         setShowError(err);
+//       }
+//     }
+
+//     fetchData();
+//   }, [showId]);
+
+//   return { showData, ShowError };
+// };
 const Show = () => {
   const { showId } = useParams();
-  const [showData, setShowData] = useState(null);
-  const [ShowError, setShowError] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getElementById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(err);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
-
+  // const { showData, ShowError } = useShowById(showId);
+  const { data: showData, error: ShowError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getElementById(showId),
+  });
   if (ShowError) {
     return <div>We have an error :{ShowError.message}</div>;
   }
