346a17dcdf6743c2b90f00a4e901895b26c2ab81
Arman <saroyasabb798@outlook.com>
2024-04-02 17:14:03 +0530

    adding the show to the startted page

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -10,3 +10,8 @@ export const searchForShow = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPerson = query => apiGet(`/search/people?q=${query}`);
 export const getElementById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+export const gerShowById = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+  const arr = await Promise.all(promises);
+  return arr;
+}; // this is used to run all the request at the same time we can also use the loops but in loops if one iteration stop the loop stop so we are using the promise where all the promise are execute at the same time
--- src/pages/started.jsx
+++ src/pages/started.jsx
@@ -1,9 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStartedShows } from '../lib/useStartedShows';
+import { gerShowById } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 
 const Started = () => {
-  const [starredShows] = useStartedShows();
-  //
-  return <div>Start page , started {starredShows.length}</div>;
+  const [starredShowsIds] = useStartedShows();
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      gerShowById(starredShows).then(result => result.map(show => ({ show }))),
+
+    refetchOnWindowFocus: false,
+  });
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+  if (starredShows?.length == 0) {
+    return <div>No Started Show Yet</div>;
+  }
+  if (starredShowsError) {
+    return <div>Error occur: {starredShowsError.message}</div>;
+  }
+
+  return <div>Shows are loading</div>;
 };
 
 export default Started;
